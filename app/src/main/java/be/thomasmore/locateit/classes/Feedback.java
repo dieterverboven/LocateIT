761277b028d1d808c03c578e624944a53158c60b
dieterverboven <dieter.verboven@outlook.com>
2019-01-22 10:08:55 +0100

    klasses & activities mappen aangemaakt

---
+++ app/src/main/java/be/thomasmore/locateit/classes/Feedback.java
@@ -0,0 +1,36 @@
+package be.thomasmore.locateit.activities.classes;
+
+public class Feedback {
+
+    private int id;
+    private int score;
+    private String beschrijving;
+
+    public Feedback(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getBeschrijving() {
+        return beschrijving;
+    }
+
+    public void setBeschrijving(String beschrijving) {
+        this.beschrijving = beschrijving;
+    }
+}
